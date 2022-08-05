e321ffb74e016404a5bf1aa190b064a85f08ab99
Mohammed Sameer Ahmed <sameerahmed976@gmail.com>
2022-08-05 21:00:48 +0530

    added tabs

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-const container = document.querySelector("container");
-const button = document.querySelector(".nav-links li a");
-const content = document.querySelector(".content");
+const container = document.querySelector(".container");
+const button = document.querySelectorAll(".nav-links li a");
+const content = document.querySelectorAll(".content");
+
+container.addEventListener("click", function (e) {
+  //   console.log(e.target.dataset.id);
+  const id = e.target.dataset.id;
+  if (id) {
+    button.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+
+    e.target.classList.add("active");
+
+    content.forEach((element) => {
+      element.classList.remove("content-active");
+    });
+
+    const contentActive = document.getElementById(id);
+
+    contentActive.classList.add("content-active");
+  }
+});
